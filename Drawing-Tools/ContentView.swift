0cf8c08e5e9f8d7d049241def177a35d4bfb6cdc
Kieran Brown <kb6627500@gmail.com>
2020-11-13 13:41:12 -0500

    Initial Commit

---
+++ Drawing-Tools/ContentView.swift
@@ -0,0 +1,21 @@
+// Swift toolchain version 5.0
+// Running macOS version 10.15
+// Created on 11/13/20.
+//
+// Author: Kieran Brown
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Drawing-Tools/Drawing_ToolsApp.swift
@@ -0,0 +1,17 @@
+// Swift toolchain version 5.0
+// Running macOS version 10.15
+// Created on 11/13/20.
+//
+// Author: Kieran Brown
+//
+
+import SwiftUI
+
+@main
+struct Drawing_ToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
